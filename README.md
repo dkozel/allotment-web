093baca14272c609538db8be2455fe6b31289192
Derek Kozel <derek@bitstovolts.com>
2025-03-03 21:52:04 +0000

    Switched to using Prisma for database

---
+++ README.md
@@ -0,0 +1,29 @@
+# How's the Allotment?
+
+The goal of this project is an elegant and minimalistic dashboard showing the recent environment from the allotment. WeatherSpark's [temperature plot](https://weatherspark.com/h/y/37834/2025/Historical-Weather-during-2025-in-Cardiff-United-Kingdom#Figures-Temperature) is the design inspiration, providing clear data viewing and browsing of the info by [day](https://weatherspark.com/h/d/37834/2024/6/26/Historical-Weather-on-Wednesday-June-26-2024-in-Cardiff-United-Kingdom#Figures-Temperature), [month](https://weatherspark.com/h/m/37834/2024/6/Historical-Weather-in-June-2024-in-Cardiff-United-Kingdom#Figures-Temperature), and [year](https://weatherspark.com/h/y/37834/2024/Historical-Weather-during-2024-in-Cardiff-United-Kingdom#Figures-Temperature).
+
+## Sensors
+
+Two Dragino LHT65N LoRa sensors ([manual](https://wiki.dragino.com/xwiki/bin/view/Main/User%20Manual%20for%20LoRaWAN%20End%20Nodes/LHT65N%20LoRaWAN%20Temperature%20%26%20Humidity%20Sensor%20Manual/#H4.13AutoSendNone-ACKmessages)) are collecting temperature and humidity data.
+
+Shed:
+ * Air temperature
+ * Air humidity
+ * Outdoor air temperature
+
+Greenhouse
+ * Air temperature
+ * Air humidity
+ * Soil temperature
+
+The data is received by The Things Stack public LoRaWAN network which calls a [webhook](https://www.thethingsindustries.com/docs/integrations/webhooks/) in this application each time a measurement is successfully uplinked.
+
+## Saving the Measurements
+
+A Flask application exposes an endpoint at `/tts/uplink/message` which receives a JSON payload for each measurement.
+
+Prisma is used to manage the storing and retrieving of measurements in a SQLite database.
+
+## Viewing the Data
+
+A React application retrieves and renders the data into plots, creating the dashboard.
--- base_app.py
+++ base_app.py
@@ -1,9 +1,10 @@
 from flask import Flask, request, jsonify
 from flask_basicauth import BasicAuth
+from prisma import Prisma, register
 
-import sqlite3
-
-api_key = "QoFahtA2ut49qCuVhZjGk2HNy2tJex"
+db = Prisma()
+db.connect()
+register(db)
 
 app = Flask(__name__)
 app.config['BASIC_AUTH_USERNAME'] = 'username'
@@ -14,38 +15,25 @@ basic_auth = BasicAuth(app)
 def index():
     return "Hello, Welcome to Derek's Allotment"
 
-@app.route('/uplink/message', methods=['POST'])
+@app.route('/tts/uplink/message', methods=['POST'])
 @basic_auth.required
 def uplink_message():
     if request.method == 'POST':
-        conn = db_connection()
-        cursor = conn.cursor()
-
         try:
             print(request.args)
-
-            # cursor.execute("INSERT INTO books (author, title) VALUES (?, ?)", (new_author, new_title))
-            # conn.commit()
-            return jsonify({"message": "Book added successfully"}), 201
+            return jsonify({"message": "Measurement added successfully"}), 201
         except sqlite3.Error as e:
             return jsonify({"error": str(e)}), 500
 
-@app.route('/uplink/payload', methods=['POST'])
+@app.route('/tts/uplink/payload', methods=['POST'])
+@basic_auth.required
 def uplink_payload():
     if request.method == 'POST':
         try:
             print(request.args)
-            return jsonify({"message": "Book added successfully"}), 201
+            return jsonify({"message": "Measurement added successfully"}), 201
         except sqlite3.Error as e:
             return jsonify({"error": str(e)}), 500
-
-def db_connection():
-    conn = None
-    try:
-        conn = sqlite3.connect('allotment_weather.sqlite')
-    except sqlite3.Error as e:
-        print("Error while connecting to SQLite database:", e)
-    return conn
 
 if __name__ == '__main__':
     app.run(debug=True)
